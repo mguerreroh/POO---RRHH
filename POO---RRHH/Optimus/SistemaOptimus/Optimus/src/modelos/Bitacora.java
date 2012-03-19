1a31f9b203ebc0b239a652e220495c7b799c890c
Marcelino Guerrero <mguerreroh@yahoo.com>
2012-03-19 13:27:30 -0500

    Getter y Setter

--- POO---RRHH/Optimus/SistemaOptimus/Optimus/src/modelos/Bitacora.java
+++ POO---RRHH/Optimus/SistemaOptimus/Optimus/src/modelos/Bitacora.java
@@ -26,6 +26,54 @@ public class Bitacora {
         this.fModifica = fModifica;
     }
 
+    public String getCodigoModulo() {
+        return codigoModulo;
+    }
+
+    public Integer getCorrelativo() {
+        return correlativo;
+    }
+
+    public Date getfCrea() {
+        return fCrea;
+    }
+
+    public Date getfModifica() {
+        return fModifica;
+    }
+
+    public String getUsCrea() {
+        return usCrea;
+    }
+
+    public String getUsModifica() {
+        return usModifica;
+    }
+
+    public void setCodigoModulo(String codigoModulo) {
+        this.codigoModulo = codigoModulo;
+    }
+
+    public void setCorrelativo(Integer correlativo) {
+        this.correlativo = correlativo;
+    }
+
+    public void setfCrea(Date fCrea) {
+        this.fCrea = fCrea;
+    }
+
+    public void setfModifica(Date fModifica) {
+        this.fModifica = fModifica;
+    }
+
+    public void setUsCrea(String usCrea) {
+        this.usCrea = usCrea;
+    }
+
+    public void setUsModifica(String usModifica) {
+        this.usModifica = usModifica;
+    }
+
     
     
 }
